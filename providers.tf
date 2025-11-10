5c8e60bf511c66b4e285da223812876ca7f5059f
Victor Sanhueza <victorsanhueza1993@gmail.com>
2025-11-10 16:54:52 -0300

    changing gcs name

--- providers.tf
+++ providers.tf
@@ -16,7 +16,7 @@ provider "google" {
 
 terraform {
   backend "gcs" {
-    bucket = "optical-depth-472215-h9-tfstate"
+    bucket = "optical-depth-472215-h9-tfstate-tfstate"
     prefix = "terraform/state"
   }
 }
