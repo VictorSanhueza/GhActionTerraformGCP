33e66425d02b01c0a669b99130891ac5917dae18
Victor Sanhueza <victorsanhueza1993@gmail.com>
2025-11-10 20:40:49 -0300

    changing disk size to test state

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "google_compute_instance" "vm" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size  = 10
+      size  = 20
       type  = "pd-balanced"
     }
   }
